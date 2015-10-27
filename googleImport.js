2fb97dc509bbefdf698a53bcbf74138ffafbf1b9
Don Richards <don@loufreyinstitute.org>
2015-10-27 15:56:37 -0400

    Daily Backup
    Working

--- googleImport.js
+++ googleImport.js
@@ -3,27 +3,24 @@
 let _temporalUndefined = {};
 let raw = _temporalUndefined;
 let VIEW_ID = _temporalUndefined;
-
 function _temporalAssertDefined(val, name, undef) {
   if (val === undef) {
     throw new ReferenceError(name + ' is not defined - temporal dead zone');
   }
-
   return true;
 }
 
 let google = require('googleapis');
 let key = require('./loufreyinstitute.json');
-
 let MongoClient = require('mongodb').MongoClient;
 let assert = require('assert');
 let ObjectId = require('mongodb').ObjectId;
 let url = 'mongodb://127.0.0.1:27017/gatc';
 
-MongoClient.connect('mongodb://127.0.0.1:27017/gatc', function(err, db) {
-  console.log("db Fired ");
+  MongoClient.connect('mongodb://127.0.0.1:27017/citizenTest', function(err, db) {
+  console.log("Database is Connected");
   if (err) throw err;
-  db.collection('users').drop();
+  //db.collection('users').remove();
 
   raw = String.raw;
   VIEW_ID = 'ga:108646183';
@@ -34,19 +31,19 @@ MongoClient.connect('mongodb://127.0.0.1:27017/gatc', function(err, db) {
       console.log(err);
       return;
     }
-
     let analytics = google.analytics('v3');
-    queryData(analytics, 1);
+    queryData(analytics, "365daysAgo", 1);
+    queryData(analytics, "180daysAgo", 1);
+    queryData(analytics, "30daysAgo", 1);
   }); // jwtClient
 
-  let queryData = function(analytics, y) {
-    console.log("queryData Fired ", y);
+  let queryData = function(analytics, days, y) {
     analytics.data.ga.get({
       auth: jwtClient,
       ids: _temporalAssertDefined(VIEW_ID, 'VIEW_ID', _temporalUndefined) && VIEW_ID,
       metrics: 'ga:sessions, ga:sessionDuration, ga:pageviews, ga:timeOnPage, ga:totalEvents, ga:uniqueEvents, ga:sessionsPerUser, ga:hits',
       dimensions: 'ga:dimension1',
-      'start-date': '365daysAgo',
+      'start-date': days,
       'end-date': 'yesterday',
       sort: '-ga:dimension1',
       type: 'dataTable',
@@ -55,30 +52,32 @@ MongoClient.connect('mongodb://127.0.0.1:27017/gatc', function(err, db) {
     }, (err, response) => {
       if (err) {
         console.log(err);
-      } // if
-
-      response.rows.forEach((rows) => {
-        db.collection('users').insert({
-          dimensions1: rows[0],
-          sessions: rows[1],
-          sessionDuration: rows[2],
-          pageviews: rows[3],
-          timeOnPage: rows[4],
-          totalEvents: rows[5],
-          uniqueEvents: rows[6],
-          sessionsPerUser: rows[7],
-          hits: rows[8],
-        }, (err, result) => {
-          assert.equal(err, null);
-        });
-      });
+        } // if
+        response.rows.forEach((rows) => {
+          var setModifier ={ $set: {}, $unset:{"password": 1} };
+          setModifier.$set[days] = {
+                  "sessions": rows[1],
+                  "sessionDuration": rows[2],
+                  "pageviews": rows[3],
+                  "timeOnPage": rows[4],
+                  "totalEvents": rows[5],
+                  "uniqueEvents": rows[6],
+                  "sessionsPerUser": rows[7],
+                  "hits": rows[8] };
+            db.collection('users').update({
+              "_gid": rows[0] },
+                setModifier,
+                (err, result) => {
+              assert.equal(err, null);
+            }); // db.collection
+          }); //response.rows.forEach
 
       if ((response.rows.length - y) >= 999) {
-        queryData(analytics, y + 1000);
+        queryData(analytics, days, y + 1000);
       } else {
         process.exit(0);
       }
     }
-);
-  };
-});
+    ); // analytics.data.ga.get
+  }; // queryData
+}); // Mongo
