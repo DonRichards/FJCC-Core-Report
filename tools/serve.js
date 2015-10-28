1e335d0dd0a1918839ddf9dc35de18abd550b222
Don Richards <don@loufreyinstitute.org>
2015-10-28 10:22:05 -0400

    Updated JSDocs

--- tools/serve.js
+++ tools/serve.js
@@ -2,7 +2,14 @@ import path from 'path';
 import cp from 'child_process';
 import task from './lib/task';
 import watch from './lib/watch';
-
+/**
+ * [Start Server]
+ * @param  {[type]} ) { const   server [Starts Core Server]
+ * @param  {[env]}   process.env) [Current Environment Developer or Production]
+ * @param  {[type]}   silent: false [Console Info Enabled]
+ * @param  {Function} });  server.once('message', message [Confirm/Fail Messages]
+ * @return {[type]}  server [init server]
+ */
 export default task('serve', () => new Promise((resolve, reject) => {
   function start() {
     const server = cp.fork(path.join(__dirname, '../core/server.js'), {
