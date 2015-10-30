207a244b1ca699524740350de41fde43c44d4fc1
Don Richards <don@loufreyinstitute.org>
2015-10-30 13:25:55 -0400

    Highly Adjusted Formatting for eslint
    Possible broke functions.

--- googleImport.js
+++ googleImport.js
@@ -1,49 +1,38 @@
 'use strict';
-/*
- * @_temporalUndefined
- */
-let _temporalUndefined = {};
-let raw = _temporalUndefined;
-let VIEW_ID = _temporalUndefined;
-function _temporalAssertDefined(val, name, undef) {
-  if (val === undef) {
-    throw new ReferenceError(name + ' is not defined - temporal dead zone');
-  }
-  return true;
-}
-
-/*
- * Importing into the LocalHost Mongodb
+/**
+ * [google description]
+ * @type {[type]}
  */
 let google = require('googleapis');
 let key = require('./authentication.json');
 let MongoClient = require('mongodb').MongoClient;
-//let assert = require('assert');
-let ObjectId = require('mongodb').ObjectId;
+let assert = require('assert');
 
 /*
  * @MongoClient
  */
-MongoClient.connect('mongodb://127.0.0.1:27017/citizen', function(err, db) {
-  if (err) throw err;
+MongoClient.connect('mongodb://127.0.0.1:27017/citizen', function mongoClient (err, db) {
+  if (err) {
+    throw err;
+  }
   /**
    * [password HASH removal]
    * @type {Object}
    */
-  console.log("Database connected, cleaning up Users data");
-  db.collection('users').update({ password : { $exists: true } }, { $unset:{ "password": 1 } } , { multi: true });
+  /* console.log("Database connected, cleaning up Users data"); */
+  db.collection('users').update({ password: { $exists: true } }, { $unset: { password: 1 } }, { multi: true });
 
   /*
    * @jwtClient
    */
-  raw = String.raw;
-  VIEW_ID = 'ga:108646183';
+  // raw = String.raw;
+  let VIEW_ID = 'ga:108646183';
   let jwtClient = new google.auth.JWT(key.client_email, null, key.private_key,
    ['https://www.googleapis.com/auth/analytics.readonly'], null);
 
-  jwtClient.authorize(function(err, tokens) {
+  jwtClient.authorize(function jwclinetAuth (err) {
     if (err) {
-      console.log(err);
+      process.stdout.write(err);
       return;
     }
     let analytics = google.analytics('v3');
@@ -51,18 +40,22 @@ MongoClient.connect('mongodb://127.0.0.1:27017/citizen', function(err, db) {
     /*
      * @contructor
      */
-    queryData(analytics, "365daysAgo", 1);
-    queryData(analytics, "180daysAgo", 1);
-    queryData(analytics, "30daysAgo", 1);
-  }); // jwtClient
+    queryData(analytics, '365daysAgo', 1);
+    queryData(analytics, '180daysAgo', 1);
+    queryData(analytics, '30daysAgo', 1);
+  });
 
-  /*
-   * @queryData
+  /**
+   * [queryData description]
+   * @param  {[type]} analytics  [description]
+   * @param  {[type]} days       [description]
+   * @param  {[type]} startIndex [description]
+   * @return {[type]}            [description]
    */
-  let queryData = function(analytics, days, y) {
+  function queryData (analytics, days, startIndex) {
     analytics.data.ga.get({
       auth: jwtClient,
-      ids: _temporalAssertDefined(VIEW_ID, 'VIEW_ID', _temporalUndefined) && VIEW_ID,
+      ids: VIEW_ID,
       metrics: 'ga:sessions, ga:sessionDuration, ga:pageviews, ga:timeOnPage, ga:totalEvents, ga:uniqueEvents, ga:sessionsPerUser, ga:hits',
       dimensions: 'ga:dimension1',
       'start-date': days,
@@ -70,66 +63,71 @@ MongoClient.connect('mongodb://127.0.0.1:27017/citizen', function(err, db) {
       sort: '-ga:dimension1',
       type: 'dataTable',
       'max-results': 1000,
-      'start-index': y,
+      'start-index': startIndex
     }, (err, response) => {
       if (err) {
-        console.log(err);
-        } // if
+        process.stdout.write(err);
+        return;
+      }
+      /**
+       * [description]
+       * @param  {[type]} (rows) [description]
+       * @return {[type]}        [description]
+       */
+      response.rows.forEach((rows) => {
+        let setModifier = { $set: {} };
 
-
-        //generator(response.rows.length);
-
-        /*
-         * @forEach.rows
-         */
-        response.rows.forEach((rows) => {
-          var setModifier ={ $set: {} };
-          setModifier.$set[days] = {
-                  "sessions": rows[1],
-                  "sessionDuration": rows[2],
-                  "pageviews": rows[3],
-                  "timeOnPage": rows[4],
-                  "totalEvents": rows[5],
-                  "uniqueEvents": rows[6],
-                  "sessionsPerUser": rows[7],
-                  "hits": rows[8] };
-            db.collection('users').update({
-              "_gid": rows[0] },
-                setModifier,
-                (err, result) => {
-              assert.equal(err, null);
-            }); // db.collection
-          }); //response.rows.forEach
+        setModifier.$set[days] = {
+          sessions: rows[1],
+          sessionDuration: rows[2],
+          pageviews: rows[3],
+          timeOnPage: rows[4],
+          totalEvents: rows[5],
+          uniqueEvents: rows[6],
+          sessionsPerUser: rows[7],
+          hits: rows[8] };
+        db.collection('users').update({
+          _gid: rows[0] },
+              setModifier,
+        (err) => {
+          assert.equal(err, null);
+        });
+      });
       process.stdout.write('.');
       /*
        * @queryDataRecursion
        */
-      if ((response.rows.length - y) >= 999) {
-        queryData(analytics, days, y + 1000);
+      if ((response.rows.length - startIndex) >= 999) {
+        queryData(analytics, days, startIndex + 1000);
       } else {
         it.next();
-        //console.log(days, response.rows.length, y);
-        //let message = progress().next;
-        //process.exit(0);
       }
     }
-    ); // analytics.data.ga.get
-  }; // queryData
-}); // Mongo
+    );
+  }
+});
 
+/**
+ * [*progress description]
+ * @yield {[type]} [description]
+ */
 function *progress () {
-    console.log('\t 1st Import Complete');
+  process.stdout.write('\t 1st Import Complete\n');
   yield 1;
-    console.log('\t 2nd Import Complete');
+  process.stdout.write('\t 2nd Import Complete\n');
   yield 2;
-    console.log('\t Final Import Complete');
+  process.stdout.write('\t Final Import Complete\n');
   return exitApp();
 }
 
-
-function exitApp(callback) {
-    setTimeout(() => callback(process.exit(0) ), 15);
+/**
+ * [exitApp description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function exitApp (callback) {
+  setTimeout(() => callback(process.exit(0)), 3000);
 }
 
-var it = progress();
+let it = progress();
 
